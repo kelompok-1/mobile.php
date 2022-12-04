b4688d5ea71e39619795b03ab399036e7695dff4
Cakra Wirajaya <cakrawirajaya@Cakras-Air.mshome.net>
2022-12-04 13:07:24 +0700

    first commit

---
+++ ambildata.php
@@ -0,0 +1,11 @@
+<?php
+include 'koneksi.php';
+
+$queryResult=$connect->query("SELECT * FROM tabel_barang");
+
+$result=array();
+while($rowData=$queryResult->fetch_assoc()){
+    $result[]=$rowData;
+}
+echo json_encode($result);
+?>
---
+++ register.php
@@ -0,0 +1,23 @@
+<?php
+$db = mysqli_connect('localhost','root','root','userdata');
+if(!$db)
+{
+	echo "Database connection failed";
+}
+$username = $_POST['username'];
+$email = $_POST['email'];
+$password = $_POST['password'];
+
+$sql = "SELECT * FROM users WHERE username = '".$username."'";
+$result = mysqli_query($db,$sql);
+$count = mysqli_num_rows($result);
+if($count == 1){
+	echo json_encode("Error");
+}else{
+	$insert = "INSERT INTO users(username,email,password) VALUES ('".$username."','".$email."','".$password."')";
+		$query = mysqli_query($db,$insert);
+		if($query){
+			echo json_encode("Success");
+		}
+}
+?>
